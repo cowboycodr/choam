8fd5703275b8bf64470ada612b35e1ded6746ca2
Kian McKenna <59800761+cowboycodr@users.noreply.github.com>
2022-01-23 14:46:44 -0600

    add: 'init' choam command

--- choam/__main__.py
+++ choam/__main__.py
@@ -7,14 +7,28 @@ class Choam:
   def __init__(self) -> None:
     pass
   
-  def new(name: str):
-    # TODO: Programmatically create folder structure
-    
+  def init(name: str):
     directory = os.getcwd()
     
     if FS.is_choam_project(directory):
-      print("\nAlready a Choam project.")
+      print("\n\tAlready a Choam project.")
       return
+    
+    with open("assets/.gitignore.txt") as f:
+      gitignore = f.read()
+    
+    template = {
+      f"\\{name}\\__main__.py": "",
+      f"\\{name}\\__init__.py": "__version__ == '0.1'",
+      f"Choam.toml": f'[package]\nname = "{name}\nversion = "0.0.1"\ndescription = ""',
+      f"README.md": f"# {name}\n#### This project was constructed with [Choam](https://github.com/cowboycodr/choam)",
+      f".gitignore": gitignore
+    }
+    
+    FS.construct_from_dict(template, directory)
+  
+  def new(name: str):
+    directory = os.getcwd()
     
     with open(os.path.abspath("choam\\assets\.gitignore.txt"), "r") as f:
       gitignore = f.read()
@@ -23,7 +37,6 @@ class Choam:
     template = {
       f"\\{name}\\{name}\\__main__.py": "",
       f"\\{name}\\{name}\\__init__.py": "__version__ == '0.1'",
-      f"\\{name}\\assets"
       f"\\{name}\\Choam.toml": f'[package]\nname = "{name}\nversion = "0.0.1"\ndescription = ""',
       f"\\{name}\\README.md": f"# {name}\n#### This project was constructed with [Choam](https://github.com/cowboycodr/choam)",
       f"\\{name}\\.gitignore": gitignore
