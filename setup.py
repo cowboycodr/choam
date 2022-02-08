3483cd5395a621318dd2d7bf62ed32dd5081b59d
Kian McKenna <kian.p.m.007@gmail.com>
2022-02-07 23:21:05 -0600

    version: 0.1.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="Choam",
-    version="0.0.7",
+    version="0.1.0",
     description="Python project scaffolder/manager",
     packages=find_packages(),
     keywords=['package', 'manager'],
-    install_requires=['typing', 'fire', 'twine', 'findimports', 'toml', 'pkg_resources', 'wheel', 'pathlib', 'choam'],
+    install_requires=['wheel', 'twine', 'toml', 'pathlib', 'typing', 'findimports', 'choam', 'fire'],
     project_urls={
         'Source': 'https://github.com/cowboycodr/choam'
     },
