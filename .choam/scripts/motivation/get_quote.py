193f06a50cfc031a613517e8f9a7befe1075dfd7
Kian McKenna <kian.p.m.007@gmail.com>
2022-02-22 11:04:15 -0600

    add: favorite quote to .choam/scripts/motivation/get_quote.py

--- .choam/scripts/motivation/get_quote.py
+++ .choam/scripts/motivation/get_quote.py
@@ -4,5 +4,7 @@ url = "https://zenquotes.io/api/random"
 
 response = requests.request("GET", url).json()[0]["q"]
 
+# Favorite: Your happiness is what truly matters most. Do what you have to do in order to be happy.
+
 print()
 print(f"\t{response}\n")
