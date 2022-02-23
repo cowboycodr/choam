6839f8c701d67ce60632c8caab8df984c0f79f16
Kian McKenna <59800761+cowboycodr@users.noreply.github.com>
2022-02-22 21:57:17 -0600

    update: choam downloads script

--- .choam/scripts/choam_downloads/get_downloads.py
+++ .choam/scripts/choam_downloads/get_downloads.py
@@ -1,17 +1,39 @@
 import numpy as np
 
 import requests
+from pathlib import Path
 from rich.console import Console
 from rich.table import Table
+
+download_history_text = Path("download_history.txt") \
+                        .read_text(encoding="utf-8")
+                    
+download_history = {}                    
+for section in download_history_text.split("\n"):
+  name, value = section.split(":")
+  
+  download_history[name] = int(value)
 
 recent_downloads_url = "https://pypistats.org/api/packages/choam/recent"
 
 recent_downloads_response = requests.get(recent_downloads_url)
 recent_downloads = recent_downloads_response.json()['data']
 
-downloads_last_day = str(recent_downloads["last_day"])
-downloads_last_week = str(recent_downloads["last_week"])
-downloads_last_month = str(recent_downloads["last_month"])
+downloads_last_day = recent_downloads["last_day"]
+downloads_last_week = recent_downloads["last_week"]
+downloads_last_month = recent_downloads["last_month"]
+
+downloads_difference = {
+  "DAY": str(int(downloads_last_day) - download_history["DAY"]),
+  "WEEK": str(int(downloads_last_week) - download_history["WEEK"]),
+  "MONTH": str(int(downloads_last_month) - download_history["MONTH"])
+}
+
+for _type, diff in downloads_difference.items():
+  if int(diff) > 0:
+    downloads_difference[_type] = "+" + diff
+  else:
+    downloads_difference[_type] = "-" + diff
 
 table = Table(title="Choam downloads")
 
@@ -19,10 +41,24 @@ table.add_column("Today", justify="center", style="yellow")
 table.add_column("This Week", justify="center", style="yellow")
 table.add_column("This Month", justify="center", style="yellow")
 
-table.add_row(downloads_last_day, 
-              downloads_last_week, 
-              downloads_last_month)
+table.add_row(str(downloads_last_day), 
+              str(downloads_last_week), 
+              str(downloads_last_month))
+
+difference_row_style = "red" if downloads_difference["DAY"].startswith("-") else "green"
+table.add_row(str(downloads_difference["DAY"]),
+              str(downloads_difference["WEEK"]),
+              str(downloads_difference["MONTH"]), style=difference_row_style)
 
 console = Console()
 print()
 console.print(table)
+
+with open("download_history.txt", "w") as file:
+  result = ""
+  
+  result += f"DAY:{downloads_last_day}"
+  result += f"\nWEEK:{downloads_last_week}"
+  result += f"\nMONTH:{downloads_last_month}"
+  
+  file.write(result)
