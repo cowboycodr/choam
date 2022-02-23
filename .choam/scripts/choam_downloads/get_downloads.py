b0511b94a21a138c2500c875af54c22b740918ec
Kian McKenna <kian.p.m.007@gmail.com>
2022-02-22 22:58:05 -0600

    update: choam downloads script

--- .choam/scripts/choam_downloads/get_downloads.py
+++ .choam/scripts/choam_downloads/get_downloads.py
@@ -32,21 +32,24 @@ downloads_difference = {
 for _type, diff in downloads_difference.items():
   if int(diff) > 0:
     downloads_difference[_type] = "+" + diff
-  else:
+  elif int(diff) < 0:
     downloads_difference[_type] = "-" + diff
 
 table = Table(title="Choam downloads")
 
-table.add_column("Today", justify="center", style="yellow")
-table.add_column("This Week", justify="center", style="yellow")
-table.add_column("This Month", justify="center", style="yellow")
+table.add_column("Stage", justify="left", style="white")
+table.add_column("Today", justify="center")
+table.add_column("This Week", justify="center")
+table.add_column("This Month", justify="center")
 
-table.add_row(str(downloads_last_day), 
+table.add_row("Current",
+              str(downloads_last_day), 
               str(downloads_last_week), 
               str(downloads_last_month))
 
-difference_row_style = "red" if downloads_difference["DAY"].startswith("-") else "green"
-table.add_row(str(downloads_difference["DAY"]),
+difference_row_style = "green" if downloads_difference["DAY"].startswith("+") else "red"
+table.add_row("Since run",
+              str(downloads_difference["DAY"]),
               str(downloads_difference["WEEK"]),
               str(downloads_difference["MONTH"]), style=difference_row_style)
 
